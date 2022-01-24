1bc129e144ca3967d3b1db9c88291efb8a8c6181
42018067 <42018067@hti.edu.eg>
2022-01-24 03:40:45 +0200

    rest framework

--- labesny/settings.py
+++ labesny/settings.py
@@ -147,4 +147,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK={
     "DEFAULT_AUTHENTICATION_CLASSES":('knox.auth.TokenAuthentication',)
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+       'rest_framework.authentication.TokenAuthentication',
+   ),
+   'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAdminUser'
+   ),
 }
