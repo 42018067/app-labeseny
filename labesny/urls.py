0cb906f214992ae068ad97eec34df7f250f35c1a
42018067 <42018067@hti.edu.eg>
2022-02-19 21:48:58 +0200

    make it better

--- labesny/urls.py
+++ labesny/urls.py
@@ -4,6 +4,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('login.urls')),
-    path('api-auth/', include('rest_framework.urls')),
     path('api/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
 ]
